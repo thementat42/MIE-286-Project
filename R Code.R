c65cf780ad14b6c32af754775f221b880c131390
Mahtasim <surhid2006@gmail.com>
2026-04-04 13:23:55 -0400

    updated R code

--- R Code.R
+++ R Code.R
@@ -20,5 +20,8 @@ qqnorm(relative_gain_feedback_response_time, main = "Relative Gain-Based Feedbac
 qqnorm(relative_loss_feedback_accuracy, main = "Relative Loss-Based Feedback Accuracy Normal Q-Q Plot")
 qqnorm(relative_loss_feedback_response_time, main = "Relative Loss-Based Feedback Response Time Normal Q-Q Plot")
 
-
+t.test(gain_feedback_accuracy[1:14], loss_feedback_accuracy, paired=T)
+t.test(relative_gain_feedback_accuracy, relative_loss_feedback_accuracy, paired=T)
+t.test(gain_feedback_response_time, loss_feedback_response_time, paired=T)
+t.test(relative_gain_feedback_response_time, relative_loss_feedback_response_time, paired=T)
   
